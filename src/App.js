77636d1e482a4f3884b33554f001d5dfc8811b5c
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-12-20 12:53:16 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPssword';
+import Tasks from './pages/Tasks';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/users/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/tasks/:userId" element={<Tasks />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
