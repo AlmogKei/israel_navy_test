ed7f42db6bfdb36a6ff1dcfb53585ca705ad1217
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-12-22 01:56:20 +0200

    Add files via upload

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL || 'postgresql://development_mbkc_user:VID2LjAmMPnNNtfbTPCkMVxycGAkLXVu@dpg-ctjktstumphs73fbs4g0-a/development_mbkc',
+  url: process.env.DATABASE_URL || 'postgresql://development_mbkc_user:VID2LjAmMPnNNtfbTPCkMVxycGAkLXVu@dpg-ctjktstumphs73fbs4g0-a.oregon-postgres.render.com/development_mbkc',
   ssl: {
     rejectUnauthorized: false
   },
   synchronize: true,
-  logging: true, // שיניתי ל-true כדי לראות את ה-queries בזמן פיתוח
+  logging: true,
   entities: ['src/entities/*.js'],
 };
