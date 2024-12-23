9737fe6db423910edd759cd50bc6bce933196abb
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-12-23 20:55:11 +0200

    Add files via upload

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL || 'postgresql://development_mbkc_user:VID2LjAmMPnNNtfbTPCkMVxycGAkLXVu@dpg-ctjktstumphs73fbs4g0-a.oregon-postgres.render.com/development_mbkc',
   ssl: {
     rejectUnauthorized: false
   },
