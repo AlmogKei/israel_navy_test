0d6bbbc5bc2a8425469d406d1141aba076465779
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-12-25 16:55:26 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,17 @@
+const { DataSource } = require('typeorm');
+const config = require('./ormconfig');
+const User = require('./entities/User');
+const Task = require('./entities/Task');
+
+// Initialize the DataSource
+const AppDataSource = new DataSource({
+    ...config,
+    entities: [
+        User,
+        Task,
+    ],
+    logging: true
+});
+
+
+module.exports = AppDataSource;
