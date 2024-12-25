6f8494cad7641de6398f6b0f76e15e1ee02204fd
Almog Kinnan <109092960+AlmogKei@users.noreply.github.com>
2024-12-25 16:51:00 +0200

    Add files via upload

--- backend/src/ormconfig.js
+++ backend/src/ormconfig.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'postgresql://development_mbkc_user:VID2LjAmMPnNNtfbTPCkMVxycGAkLXVu@dpg-ctjktstumphs73fbs4g0-a/development_mbkc',
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USER || 'development_mbkc_user',
-  password: process.env.DB_PASSWORD || 'VID2LjAmMPnNNtfbTPCkMVxycGAkLXVu',
-  database: process.env.DB_NAME || 'development_mbkc',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'development',
   synchronize: true,
   logging: false,
   entities: ['src/entities/*.js'],
